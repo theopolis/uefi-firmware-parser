9335a88d198bb98f489b395391035f497d12dec2
Teddy Reed <teddy@casualhacking.io>
2023-06-15 22:17:47 -0400

    ci: Use GitHub actions for CI (#119)

--- setup.py
+++ setup.py
@@ -94,9 +94,10 @@ setup(
         'Intended Audience :: System Administrators',
         'Topic :: Security',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     keywords="security uefi firmware parsing bios",
 )
